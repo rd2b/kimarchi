f8503a5f21f0962dc43d53690d328a2537680531
Remi Debay <rdebay@rdebay-vdi.vm.vdi.s1.p.fti.net>
2014-06-25 15:50:24 +0200

    Gestion des ip

--- groups/default/etc/openvpn/init.sh
+++ groups/default/etc/openvpn/init.sh
@@ -2,5 +2,7 @@
                                                                                 
 device="$1"                                                                     
                                                                                 
-/sbin/ifconfig $device 192.168.0.151 netmask 255.255.255.0 
-
+ifconfig="/sbin/ifconfig"                                                       
+                                                                                
+test "$HOSTNAME" = "ks01" && $ifconfig $device 192.168.0.151 netmask 255.255.255.0      
+test "$HOSTNAME" = "ks03" && $ifconfig $device 192.168.0.153 netmask 255.255.255.0    
