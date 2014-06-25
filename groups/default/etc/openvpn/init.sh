bd72bb6f77cd96567a74365afd540b8493cfe8d0
Remi Debay <rdebay@rdebay-vdi.vm.vdi.s1.p.fti.net>
2014-06-25 16:10:10 +0200

    Suppression fichier

--- groups/default/etc/openvpn/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-                                                                                
-device="$1"                                                                     
-                                                                                
-ifconfig="/sbin/ifconfig"                                                       
-                                                                                
-test "$HOSTNAME" = "ks01" && $ifconfig $device 192.168.0.151 netmask 255.255.255.0      
-test "$HOSTNAME" = "ks03" && $ifconfig $device 192.168.0.153 netmask 255.255.255.0    
